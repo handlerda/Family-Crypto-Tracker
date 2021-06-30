93592ae14a1d0f98fc00302de30d0f3afe6df9cc
Davis <handlerdavis@gmail.com>
2021-06-30 10:39:03 -0600

    added navbar

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,34 +1,280 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import ProfileButton from './ProfileButton';
-import LoginFormModal from '../LoginFormModal';
-import './Navigation.css';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
+import ProfileButton from "./ProfileButton";
+import LoginFormModal from "../LoginFormModal";
+import { Fragment } from "react";
+import { Disclosure, Menu, Transition } from "@headlessui/react";
+import { BellIcon, MenuIcon, XIcon } from "@heroicons/react/outline";
+import { PlusIcon } from "@heroicons/react/solid";
+import { CogIcon } from "@heroicons/react/solid";
 
-function Navigation({ isLoaded }){
-  const sessionUser = useSelector(state => state.session.user);
+import "./Navigation.css";
 
-  let sessionLinks;
-  if (sessionUser) {
-    sessionLinks = (
-      <ProfileButton user={sessionUser} />
-    );
-  } else {
-    sessionLinks = (
-      <>
-        <LoginFormModal />
-        <NavLink to="/signup">Sign Up</NavLink>
-      </>
-    );
+function Navigation({ isLoaded }) {
+  // const sessionUser = useSelector(state => state.session.user);
+
+  // let sessionLinks;
+  // // render navbar with loggedin features
+  // if (sessionUser) {
+  //   sessionLinks = (
+  //     <ProfileButton user={sessionUser} />
+  //   );
+  // } else {
+  //   sessionLinks = (
+  //     <>
+  //       <LoginFormModal />
+  //       <NavLink to="/signup">Sign Up</NavLink>
+  //     </>
+  //   );
+  // }
+
+  // return (
+  //   <ul>
+  //     <li>
+  //       <NavLink exact to="/">Home</NavLink>
+  //       {isLoaded && sessionLinks}
+  //     </li>
+  //   </ul>
+  // );
+
+  const unauthenticatedNavigation = [
+    { name: "What we do", href: "#", current: false },
+    { name: "Why crypfam", href: "#", current: false },
+  ];
+
+  const unauthenticatedButtons = [
+    { name: "Login", href: "#", current: false },
+    { name: "Signup", href: "#", current: false },
+    { name: "Demo user", href: "#", current: false },
+  ];
+
+  //nav links for a logged in user
+  const authenticatedNavigation = [
+    { name: "Family Dashboard", href: "#", current: false },
+    { name: "My Dashboard", href: "#", current: false },
+    { name: "Transactions", href: "#", current: false },
+    { name: "Balances", href: "#", current: false },
+  ];
+  //user settings for a logged in user
+  const userNavigation = [
+    { name: "Your Profile", href: "#" },
+    { name: "Settings", href: "#" },
+    { name: "Sign out", href: "#" },
+  ];
+
+  function classNames(...classes) {
+    return classes.filter(Boolean).join(" ");
   }
 
+  // sessionUser
+  const sessionUser = useSelector((state) => state.session.user);
+  console.log(sessionUser);
+
   return (
-    <ul>
-      <li>
-        <NavLink exact to="/">Home</NavLink>
-        {isLoaded && sessionLinks}
-      </li>
-    </ul>
+    isLoaded && (
+      <Disclosure as="nav" className="bg-purple-600">
+        {({ open }) => (
+          <>
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+              <div className="flex justify-between h-16">
+                <div className="flex">
+                  <div className="-ml-2 mr-2 flex items-center md:hidden">
+                    {/* Mobile menu button */}
+                    <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                      <span className="sr-only">Open main menu</span>
+                      {open ? (
+                        <XIcon className="block h-6 w-6" aria-hidden="true" />
+                      ) : (
+                        <MenuIcon
+                          className="block h-6 w-6"
+                          aria-hidden="true"
+                        />
+                      )}
+                    </Disclosure.Button>
+                  </div>
+                  <div className="flex-shrink-0 flex items-center">
+                    <span className="hidden font-bold text-pink-200 lg:block w-auto text-2xl">
+                      Crypfam
+                    </span>
+                  </div>
+                  <div className="hidden md:ml-6 md:flex md:items-center md:space-x-4">
+                    {sessionUser
+                      ? authenticatedNavigation.map((item) => (
+                          <a
+                            key={item.name}
+                            href={item.href}
+                            className={classNames(
+                              item.current
+                                ? "bg-gray-900 text-white"
+                                : "text-white hover:bg-gray-700 hover:text-white",
+                              "px-3 py-2 rounded-md text-sm font-medium"
+                            )}
+                            aria-current={item.current ? "page" : undefined}
+                          >
+                            {item.name}
+                          </a>
+                        ))
+                      : unauthenticatedNavigation.map((item) => (
+                          <a
+                            key={item.name}
+                            href={item.href}
+                            className={classNames(
+                              item.current
+                                ? "bg-gray-900 text-white"
+                                : "text-white hover:bg-gray-700 hover:text-white",
+                              "px-3 py-2 rounded-md text-sm font-medium"
+                            )}
+                            aria-current={item.current ? "page" : undefined}
+                          >
+                            {item.name}
+                          </a>
+                        ))}
+                  </div>
+                </div>
+                <div className="flex items-center">
+                  <div className="flex-shrink-0">
+                    {sessionUser ? (
+                      <button
+                        type="button"
+                        className="relative inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-pink-500 hover:bg-pink-600 focus:outline-none focus:ring-2 focus:ring-offset-2"
+                      >
+                        <PlusIcon
+                          className="-ml-1 mr-2 h-5 w-5"
+                          aria-hidden="true"
+                        />
+                        <span>Connect a crypto wallet</span>
+                      </button>
+                    ) : (
+                      unauthenticatedButtons.map((item) => {
+                        return (
+                          <button
+                            type="button"
+                            className="relative inline-flex items-center mx-4 px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-pink-500 hover:bg-pink-600 focus:outline-none focus:ring-2 focus:ring-offset-2"
+                          >
+                            <span>{item.name}</span>
+                          </button>
+                        );
+                      })
+                    )}
+                  </div>
+
+                  {sessionUser && (
+                    <div className="hidden md:ml-4 md:flex-shrink-0 md:flex md:items-center">
+                      {/* Profile dropdown */}
+                      <Menu as="div" className="ml-3 relative">
+                        {({ open }) => (
+                          <>
+                            <div>
+                              <Menu.Button className="flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                                <span className="sr-only">Open user menu</span>
+                                <CogIcon className="h-8 w-8 rounded-full text-white" />
+                              </Menu.Button>
+                            </div>
+                            <Transition
+                              show={open}
+                              as={Fragment}
+                              enter="transition ease-out duration-200"
+                              enterFrom="transform opacity-0 scale-95"
+                              enterTo="transform opacity-100 scale-100"
+                              leave="transition ease-in duration-75"
+                              leaveFrom="transform opacity-100 scale-100"
+                              leaveTo="transform opacity-0 scale-95"
+                            >
+                              <Menu.Items
+                                static
+                                className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+                              >
+                                {userNavigation.map((item) => (
+                                  <Menu.Item key={item.name}>
+                                    {({ active }) => (
+                                      <a
+                                        href={item.href}
+                                        className={classNames(
+                                          active ? "bg-gray-100" : "",
+                                          "block px-4 py-2 text-sm text-gray-700"
+                                        )}
+                                      >
+                                        {item.name}
+                                      </a>
+                                    )}
+                                  </Menu.Item>
+                                ))}
+                              </Menu.Items>
+                            </Transition>
+                          </>
+                        )}
+                      </Menu>
+                    </div>
+                  )}
+                </div>
+              </div>
+            </div>
+
+            <Disclosure.Panel className="md:hidden">
+              <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+                {sessionUser
+                  ? authenticatedNavigation.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className={classNames(
+                          item.current
+                            ? "bg-gray-900 text-white"
+                            : "text-white hover:bg-gray-700 hover:text-white",
+                          "block px-3 py-2 rounded-md text-base font-medium"
+                        )}
+                        aria-current={item.current ? "page" : undefined}
+                      >
+                        {item.name}
+                      </a>
+                    ))
+                  : unauthenticatedNavigation.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className={classNames(
+                          item.current
+                            ? "bg-gray-900 text-white"
+                            : "text-white hover:bg-gray-700 hover:text-white",
+                          "block px-3 py-2 rounded-md text-base font-medium"
+                        )}
+                        aria-current={item.current ? "page" : undefined}
+                      >
+                        {item.name}
+                      </a>
+                    ))}
+              </div>
+              {sessionUser && (
+                <div className="pt-4 pb-3 border-t border-gray-700">
+                  <div className="flex items-center px-5 sm:px-6">
+                    <div>
+                      <div className="text-base font-medium text-white">
+                        {sessionUser.name}
+                      </div>
+                      <div className="text-sm font-medium text-white">
+                        {sessionUser.email}
+                      </div>
+                    </div>
+                  </div>
+                  <div className="mt-3 px-2 space-y-1 sm:px-3">
+                    {userNavigation.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className="block px-3 py-2 rounded-md text-base font-medium text-white hover:text-white hover:bg-gray-700"
+                      >
+                        {item.name}
+                      </a>
+                    ))}
+                  </div>
+                </div>
+              )}
+            </Disclosure.Panel>
+          </>
+        )}
+      </Disclosure>
+    )
   );
 }
 
