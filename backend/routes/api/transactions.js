69b7f2d60096151400ffe5b800b9a3071743dd66
Davis <handlerdavis@gmail.com>
2021-07-24 07:45:19 -0600

    fixed transactions backend for non owner accounts

--- backend/routes/api/transactions.js
+++ backend/routes/api/transactions.js
@@ -3,26 +3,33 @@ const asyncHandler = require("express-async-handler");
 const { restoreUser } = require("../../utils/auth");
 const router = express.Router();
 const Zabo = require("zabo-sdk-js");
-
+const { Account, User } = require("./../../db/models");
 router.get(
   // send
   "/account/:account",
   restoreUser,
   asyncHandler(async (req, res, next) => {
     //get zabo info
-    console.log(req);
     const zaboAccountId = req.params.account;
-
-    const zaboUserId = req.user.zaboId;
-    console.log(`here is the zaboUserId`, zaboUserId);
-    //init zabo
+    //get the accountId
+    const account = await Account.findOne({
+      where: {
+        zaboId: zaboAccountId,
+      },
+    });
+    // get user associated with account
+    const user = await User.findOne({
+      where: {
+        id: account.userId,
+      },
+    });
     const zabo = await Zabo.init({
       apiKey: process.env.ZABO_PUBLIC_KEY,
       secretKey: process.env.ZABO_PRIVATE_KEY,
       env: "sandbox",
     });
     const transactions = await zabo.transactions.getList({
-      userId: zaboUserId,
+      userId: user.zaboId,
       accountId: zaboAccountId,
     });
     res.json(transactions);
